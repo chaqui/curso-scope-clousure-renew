132c2783d83a1c82d6a5bdcbf98c3bef9e3f7367
josue chaqui <josue.chaqui@gmail.com>
2022-07-12 08:09:09 -0600

    scope global

---
+++ src/scope/global.js
@@ -0,0 +1,15 @@
+var a; //declarando
+var b = 'b' //declarando y asignando
+b = 'bb' //reasignando]
+var a = 'aa' //redeclarando
+
+
+//Global Scope
+var fruits = 'apple'
+
+function bestFruit() {
+    console.log(fruits)
+}
+
+
+bestFruit();
